157dcff9f1ff70bfaef1c333fac8a76b9163fc9d
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2024-08-05 09:31:50 +0530

    home banner part

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,38 +1,59 @@
 import React from 'react';
-import Slider from "react-slick";
+import {Swiper,SwiperSlide} from 'swiper/react';
+import slide1 from '../../assets/images/slideshow/Beige and Gray Elegant Jewerly Photo Etsy Shop Cover  (1).png'
+import slide2 from '../../assets/images/slideshow/Beige and Gray Elegant Jewerly Photo Etsy Shop Cover  (2).png'
+import slide3 from '../../assets/images/slideshow/Beige and Gray Elegant Jewerly Photo Etsy Shop Cover  (3).png'
+import slide4 from '../../assets/images/slideshow/Beige and Gray Elegant Jewerly Photo Etsy Shop Cover .png'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Navigation } from '@mui/icons-material';
+import { Navigation,Autoplay } from 'swiper/modules';
 
 const HomeBanner = () => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows:true,
-    autoplay:true
-  };
-
+  
   return (
-    <div className="homeBannerSection">
-      <Slider {...settings}>
-        <div className='item'>
-            <img src="https://img.lazcdn.com/us/domino/421bb745-67b4-460b-855c-e529ddc6dd8f_SG-1976-688.jpg_2200x2200q80.jpg" className='w-100'/>
-        </div>
-        <div className='item'>
-            <img src="https://img.lazcdn.com/us/domino/04643538-1382-4501-859b-e9d3f5374771_SG-1976-688.jpg_2200x2200q80.jpg" className='w-100'/>
-        </div>
-        <div className='item'>
-            <img src="https://img.lazcdn.com/us/domino/de59fc3f-512e-44f8-b3dc-a02b7d33f587_SG-1976-688.jpg_2200x2200q80.jpg" className='w-100'/>
-        </div>
-        <div className='item'>
-            <img src="https://img.lazcdn.com/us/domino/e622e546-ec2d-48c8-8f6b-38eccd9ea8f6_SG-1976-688.jpg_2200x2200q80.jpg" className='w-100'/>
-        </div>
-      </Slider>
+    <div className="container mt-3">
+      <div className="homeBannerSection">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={15}
+          navigation={true}
+          loop={false}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Navigation, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="item">
+              <img src={slide1} className="w-100" alt="Slide 1" />
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="item">
+              <img src={slide2} className="w-100" alt="Slide 1" />
+            </div>
+          </SwiperSlide>
+          
+          <SwiperSlide>
+            <div className="item">
+              <img src={slide3} className="w-100" alt="Slide 1" />
+            </div>
+          </SwiperSlide>
+          
+          <SwiperSlide>
+            <div className="item">
+              <img src={slide4} className="w-100" alt="Slide 1" />
+            </div>
+          </SwiperSlide>
+
+        </Swiper>
+      </div>
     </div>
   );
 };
+
 
 export default HomeBanner;
